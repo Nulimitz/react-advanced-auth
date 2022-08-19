14730bc9b94493ccddc6ac57c91f372fde8cc0ad
Mark Winters <nulimitz360@gmail.com>
2022-08-19 17:34:17 -0600

    Cleaned up the client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,5 @@
-import "./App.css";
-
 function App() {
-  return (
-    <>
-      <div className="">Home Page</div>
-    </>
-  );
+  return <div className="">Home Page</div>;
 }
 
 export default App;
