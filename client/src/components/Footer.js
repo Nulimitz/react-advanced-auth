2ae97fa09a87c562b2ce6900addbaff5ea7891f8
Mark Winters <nulimitz360@gmail.com>
2022-08-26 12:00:59 -0600

    Added logo to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,10 @@
+import Logo from "../assets/images/logo.png";
 function Footer() {
   return (
     <footer>
       <div className="container text-center">
-        &copy; 2022 All right reserved
+        &copy; 2022 <img src={Logo} width="24" height="24" alt="Home" /> React
+        App
       </div>
     </footer>
   );
